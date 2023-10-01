c811ab2c5e4fd62493e00e1ed98e60c3643d2128
Pedro Sartori <pedrosartdr@gmail.com>
2023-10-01 12:13:56 -0300

    tratamento de valores inconsistentes

--- pre_processamento.R
+++ pre_processamento.R
@@ -1,7 +1,18 @@
 #setwd('C:/Users/Lenovo/Documents/Udemy_Machine_Learning_R')
 library(dplyr)
 
-df <- read.csv('credit_data.csv')
-df$clientid = NULL
-glimpse(df)
-summary(df)
+base <- read.csv('credit_data.csv')
+base$clientid = NULL
+glimpse(base)
+summary(base)
+
+idade_invalida = base[base$age < 0 & !is.na(base$age),]
+
+# apagar somente os registros com problema
+base = base[base$age > 0,]
+
+# preencher os dados manualmente
+
+# calcular a média da idade
+mean_age = mean(base$age[base$age > 0], na.rm = TRUE)
+base$age = ifelse(base$age < 0, mean_age, base$age)
